2085f78cb45b73cf96c5fdf55b4ae7df018452a3
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-07-30 09:14:38 +0700

    update dependencies sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
-import "./styles/index.scss";
+import "./styles/style.scss";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <main></main>
+      <main>
+        <h1 className='title'>Title</h1>
+      </main>
       <Footer />
     </div>
   );
